bb1dba8331e8a9bd58bc8b46b37786bef97487dd
NSMarcely <hms75jrxtx@privaterelay.appleid.com>
2026-06-18 17:06:57 -0300

    :spaarkles: feat(repository): cria SolicitacaoRepository

--- turing/src/main/java/br/com/seuespacounb/turing/repository/SolicitacaoRepository.java
+++ turing/src/main/java/br/com/seuespacounb/turing/repository/SolicitacaoRepository.java
@@ -1,4 +1,23 @@
 package br.com.seuespacounb.turing.repository;
 
-public interface SolicitacaoRepository {
+
+import br.com.seuespacounb.turing.entity.Solicitacao;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+
+@Repository
+public interface SolicitacaoRepository extends JpaRepository<Solicitacao, Long>, JpaSpecificationExecutor<Solicitacao> {
+
+    @Query("SELECT COUNT(solicitacao)>0 FROM Solicitacao solicitacao " +
+            "WHERE solicitacao.dataSolicitacao <= :dataLimite " +
+            "AND solicitacao.horario.id = :horarioId")
+    boolean conflitoSolicitacao(
+            @Param("dataLimite") LocalDateTime dataLimite,
+            @Param("horarioId") Long horarioId
+    );
 }
