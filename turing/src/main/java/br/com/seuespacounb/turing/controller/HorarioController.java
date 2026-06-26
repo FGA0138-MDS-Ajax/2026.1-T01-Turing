109486b1f651fef5bc66f34cadcb0ac64f7b0baf
Pedro Galdino <pedrogaldinob@gmail.com>
2026-06-26 11:13:41 -0300

    Alterações no Usuário
    Fiz algumas alterações: Arrumei o campo que fazia o usuario escolher o tipoUsuario, agora todo usuário criado vai ser criado direto como CLIENTE; Criei um AdmSeeder que verifica se existe um adm no sistema ja, e caso não exista ele cria um como default, com tudo definido padrão; Além disso arrumei os erros que ficaram pendentes quando foi dado merge das branches pra developer-backend, agora tudo voltado ao normal. Agora tem que refazer todos os testes pra ter 100% de certeza que ta tudo passandon certo

--- turing/src/main/java/br/com/seuespacounb/turing/controller/HorarioController.java
+++ turing/src/main/java/br/com/seuespacounb/turing/controller/HorarioController.java
@@ -2,36 +2,15 @@ package br.com.seuespacounb.turing.controller;
 
 import br.com.seuespacounb.turing.dto.request.HorarioSalaRequestDTO;
 import br.com.seuespacounb.turing.dto.response.HorarioSalaResponseDTO;
-import java.time.DayOfWeek;
-import java.time.LocalDate;
-import java.time.LocalTime;
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
-import br.com.seuespacounb.turing.dto.AtualizarStatusRequest;
-import br.com.seuespacounb.turing.dto.HorarioSalaRequestDTO;
-import br.com.seuespacounb.turing.dto.HorarioSalaResponseDTO;
 import br.com.seuespacounb.turing.exception.ConflictException;
-import br.com.seuespacounb.turing.exception.HttpMessageNotReadableException;
 import br.com.seuespacounb.turing.exception.NotFoundException;
 import br.com.seuespacounb.turing.service.HorarioSalaService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -42,36 +21,26 @@ public class HorarioController {
     private final HorarioSalaService horarioSalaService;
 
     @GetMapping("/horarios/sala/{salaId}")
-    public ResponseEntity<List<HorarioSalaResponseDTO>> buscarHorarioPorSala(
-            @PathVariable Long salaId) throws NotFoundException, HttpRequestMethodNotSupportedException {
+    public ResponseEntity<List<HorarioSalaResponseDTO>> buscarHorariosPorSala(
+            @PathVariable Long salaId) throws NotFoundException {
         return ResponseEntity.ok(horarioSalaService.listarHorariosPorSala(salaId));
     }
 
     @GetMapping("/horarios/{id}")
     public ResponseEntity<HorarioSalaResponseDTO> buscarPorId(
-            @PathVariable Long id) throws NotFoundException, HttpRequestMethodNotSupportedException {
+            @PathVariable Long id) throws NotFoundException {
         return ResponseEntity.ok(horarioSalaService.buscarPorId(id));
-    @GetMapping("/todos")
-    public ResponseEntity<List<HorarioSalaResponseDTO>> buscarTodosHorarios() {
-        List<HorarioSalaResponseDTO> horarios = horarioSalaService.listarTodosHorarios();
-        return ResponseEntity.ok(horarios);
-    }
-
-    @PostMapping
-    public ResponseEntity<HorarioSalaResponseDTO> salvar(@RequestBody @Valid HorarioSalaRequestDTO horarioSalaRequestDTO) throws ConflictException, NotFoundException{
-        HorarioSalaResponseDTO horarioSalvo =  horarioSalaService.salvarHorario(horarioSalaRequestDTO);
-        return ResponseEntity.status(HttpStatus.CREATED).body(horarioSalvo);
     }
 
     @PostMapping("/horarios")
     public ResponseEntity<HorarioSalaResponseDTO> salvar(
-            @Valid @RequestBody HorarioSalaRequestDTO dto) throws NotFoundException, ConflictException, HttpMessageNotReadableException, HttpRequestMethodNotSupportedException {
+            @Valid @RequestBody HorarioSalaRequestDTO dto) throws NotFoundException, ConflictException {
         return ResponseEntity.status(HttpStatus.CREATED).body(horarioSalaService.salvarHorario(dto));
     }
 
     @DeleteMapping("/horarios/{id}")
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void excluir(@PathVariable Long id) throws NotFoundException, ConflictException, HttpRequestMethodNotSupportedException {
+    public void excluir(@PathVariable Long id) throws NotFoundException, ConflictException {
         horarioSalaService.excluirHorario(id);
     }
 }
