3abef8dfe2d7e1b3fbf06a5a51fe1e1438e4b58d
NSMarcely <hms75jrxtx@privaterelay.appleid.com>
2026-06-28 20:48:52 -0300

    :sparkles: feat(service): cria GoogleCalendarService
    Co-authored-by: Gabriel Vieira <Gabriel.vieira.santos.110@gmail.com>

--- turing/src/main/java/br/com/seuespacounb/turing/service/GoogleCalendarService.java
+++ turing/src/main/java/br/com/seuespacounb/turing/service/GoogleCalendarService.java
@@ -1,4 +1,53 @@
 package br.com.seuespacounb.turing.service;
 
+import br.com.seuespacounb.turing.dto.response.GoogleCalendarResponse;
+import br.com.seuespacounb.turing.entity.Solicitacao;
+import br.com.seuespacounb.turing.entity.StatusSolicitacao;
+import br.com.seuespacounb.turing.exception.BadRequestException;
+import br.com.seuespacounb.turing.exception.NotFoundException;
+import br.com.seuespacounb.turing.repository.SolicitacaoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.util.UriComponentsBuilder;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+@Service
+@RequiredArgsConstructor
 public class GoogleCalendarService {
+    private final SolicitacaoRepository solicitacaoRepository;
+
+    private Solicitacao pegarSolicitacao(Long solicitacaoId) throws NotFoundException,BadRequestException{
+        Solicitacao solicitacao = solicitacaoRepository
+                .findByIdAndStatus(solicitacaoId, StatusSolicitacao.APROVADA)
+                .orElseThrow(()-> new NotFoundException("A solicitação não foi encontrada ou não foi aprovada."));
+        if(solicitacao.getDataUso().isBefore(LocalDate.now()))
+            throw new BadRequestException("Não é possível exportar reservas com data anterior ao dia atual.");
+        return solicitacao;
+    }
+
+    private String formatarDataHorario(LocalDate dataSolicitacao, LocalTime horarioSolicitacao){
+        LocalDateTime dateTime = LocalDateTime.of(dataSolicitacao, horarioSolicitacao);
+        return dateTime.format(DateTimeFormatter.ofPattern("yyyyMMdd'T'HHmmss"));
+    }
+
+    public GoogleCalendarResponse montarUrlGoogleCalendar(Long solicitacaoId) throws NotFoundException{
+        Solicitacao solicitacao = pegarSolicitacao(solicitacaoId);
+        String titulo = "Sua sala reservada: " + solicitacao.getHorarioSala().getSala().getNome();
+        String inicioHora = formatarDataHorario(solicitacao.getDataUso(), solicitacao.getHorarioSala().getInicioHora());
+        String fimHora = formatarDataHorario(solicitacao.getDataUso(), solicitacao.getHorarioSala().getFimHora());
+        String localizacao = solicitacao.getHorarioSala().getSala().getLocalizacao();
+        String linkCalendar = UriComponentsBuilder
+                .fromUriString("https://calendar.google.com/calendar/render")
+                .queryParam("action", "TEMPLATE")
+                .queryParam("text", titulo)
+                .queryParam("dates", inicioHora + "/" + fimHora)
+                .queryParam("ctz", "America/Sao_Paulo")
+                .queryParam("details", solicitacao.getMotivo())
+                .queryParam("location", localizacao)
+                .toUriString();
+        return new GoogleCalendarResponse(linkCalendar);
+    }
 }
